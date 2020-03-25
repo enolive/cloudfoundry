acffb77adf12526676d7936e6a345a84eb82f7cd
Christoph Welcz <christoph@welcz.de>
2020-03-25 11:13:17 +0100

    formatting

--- hello-express/server.js
+++ hello-express/server.js
@@ -2,4 +2,6 @@ import {app} from './routes';
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`listening on port ${port}...`));
+app.listen(port,
+  () => console.log(`listening on port ${port}...`)
+);
